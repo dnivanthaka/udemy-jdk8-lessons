2950aceef0415381c331869587efaaba42ab3c6e
Dinusha Nivanthaka Amerasinghe <dinusha@dinusha-HP-Pro-3130-Microtower-PC>
2016-09-01 18:13:14 +0530

    Added the Section 4 Lab Work

---
+++ Section3/AcmeOrderSystem/src/Order.java
@@ -0,0 +1,34 @@
+public class Order {
+	MyDate orderDate;
+	double orderAmount = 0.00;
+	String customer;
+	String product;
+	int quantity;
+	static double taxRate = 0.05;
+	
+	public Order(MyDate d, double amt, String c, String p, int q){
+		orderDate=d;
+		orderAmount=amt;
+		customer=c;
+		product=p;
+		quantity=q;
+	}
+	
+	public static void setTaxRate(double newRate){
+		taxRate = newRate;
+	}
+	
+	public static void computeTaxOn(double amount){
+		System.out.println("The tax for "+amount+" is "+(amount * Order.taxRate));
+	}
+	
+	public double computeTax(){
+		System.out.println("The tax for this amount is "+(orderAmount * Order.taxRate));
+		
+		return orderAmount * Order.taxRate;
+	}
+	
+	public String toString(){
+		return quantity + " ea. " + product + " for " + customer; 
+	}
+}
