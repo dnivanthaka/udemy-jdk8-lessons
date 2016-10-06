ec2f33b6b109b62bd82db6bfb6916aa129e43b4d
dnivanthaka <nivanthaka@gmail.com>
2016-10-06 23:29:47 +0530

    Added the Lab 9 solutions

---
+++ Section3/AcmeOrderSystem/src/com/acme/domain/Liquid.java
@@ -0,0 +1,35 @@
+package com.acme.domain;
+
+import com.acme.domain.Good.UnitOfMeasureType;
+
+public class Liquid extends Good {
+	private double radius;
+
+	
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	public Liquid(String name, int modelNumber, double height, UnitOfMeasureType uoM, 
+			boolean flammable, double wgtPerUoM, double radius){
+		super(name, modelNumber, height, uoM, 
+				flammable, wgtPerUoM);
+		
+		this.radius = radius;
+		
+	}
+	
+	public double volume(){
+		return Math.PI * radius * radius * getHeight();
+	}
+	
+	public String toString(){
+		return super.toString() + " (liquid) " + volume() + " " + getUnitOfMeasure();
+	}
+	
+	
+}
